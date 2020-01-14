92fa0b9fa6d393ae07a1ff445560771c22d1760b
MalamaKangwa <Malama.Kangwa@szi.gov.zm>
2020-01-14 21:31:11 +0200

    Feature: Added Sum Feature

---
+++ BasicOperations/Addition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicOperations
+{
+    public class Addition
+    {
+        public static dynamic Sum(dynamic a, dynamic b)
+        {
+            return a + b;
+        }
+
+        public static dynamic Sum(dynamic arrayA)
+        {
+            dynamic c = 0;
+            foreach (dynamic a in arrayA)
+            {
+                c = Sum(a, c);
+            }
+            return c;
+        }
+    }
+}
