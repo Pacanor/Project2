8fe579d6f32bc39fc17f1ed967b694606084f122
abigail <abigailnankamba@gmail.com>
2020-01-14 22:09:59 +0200

    Feature: Create a Static, Object, Interface and Test for Difference

---
+++ Subtract/Isubtractedby.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Subtract
+{
+    public interface ISubtractby
+    {
+
+        public dynamic Subtracted(dynamic a, dynamic b);
+    }
+}
---
+++ Subtract/Subtractedby.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BasicOperations;
+namespace Subtract
+{
+    public class Subtractby : ISubtractby
+    {
+        public dynamic results;
+        public dynamic Subtracted(dynamic a, dynamic b)
+        {
+            results = Subtraction.Subtract(a, b);
+            return results;
+        }
+    }
+}
