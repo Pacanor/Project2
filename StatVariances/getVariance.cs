c90c9e2c34781e73b68a2cfadb055c65956a4f33
MalamaKangwa <Malama.Kangwa@szi.gov.zm>
2020-01-15 18:24:10 +0200

    Feature: Re-Added Variance

---
+++ StatVariances/getVariance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using StatisticOperations;
+
+namespace StatVariances
+{
+    public class getVariance : IVariance
+    {
+        public static dynamic result;
+
+        public dynamic Variance(dynamic arrayA)
+        {
+            result = StatVariance.Variance(arrayA);
+            return result;
+        }
+
+    }
+}
