c20ed254df05bd13816730dd502a6500e5757873
abigail <abigailnankamba@gmail.com>
2020-01-15 09:42:08 +0200

    Feature: Created a Static, Object, Interface and Test for a Square

---
+++ SquareI/ISquared1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquareI
+{
+    public interface ISquared1
+    {
+        public dynamic Squarea(dynamic a);
+    }
+}
