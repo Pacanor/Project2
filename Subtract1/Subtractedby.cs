edffe1840508348548b27d9876e954ea9e35a7d4
abigail <abigailnankamba@gmail.com>
2020-01-15 10:49:19 +0200

    Fix: Name fix

---
+++ Subtract1/Subtractedby.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BasicOperations;
+namespace Subtract1
+{
+    public class Subtractedby : ISubtractedby
+    {
+        public dynamic results;
+        public dynamic Subtracted(dynamic a, dynamic b)
+        {
+            results = Subtraction.Subtract(a, b);
+            return results;
+        }
+        public dynamic Subtracted(dynamic a)
+        {
+            results = Subtraction.Subtract(a);
+            return results;
+        }
+    }
+}
