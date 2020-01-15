6d307c518f9623b2175567202f1bec83fcc5a934
pacanor <papiasb@gmail.com>
2020-01-15 07:49:54 +0200

    Feature: added static functions to find the mean of an array of values

---
+++ StatisticOperations/StatMean.cs
@@ -0,0 +1,28 @@
+﻿
+namespace StatisticOperations
+{
+    public class StatMean
+    {
+        public static dynamic Mean(int[] values)
+        {
+            var sum = 0;
+            foreach(int a in values)
+            {
+                sum += a;
+            }
+            var mean = sum/ values.Length;
+            return mean;
+        }
+
+        public static dynamic Mean(double[] values)
+        {
+            var sum = 0.0;
+            foreach (double a in values)
+            {
+                sum += a;
+            }
+            var mean = sum / values.Length;
+            return mean;
+        }
+    }
+}
