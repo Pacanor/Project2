b0689fdd1176b2d71c60551c4a71941a90195a9c
MalamaKangwa <Malama.Kangwa@szi.gov.zm>
2020-01-15 18:25:29 +0200

    Task: Added Variance Tests

---
+++ StatisticOperationsTests/StatVarianceTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatisticOperations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticOperations.Tests
+{
+    [TestClass()]
+    public class StatVarianceTests
+    {
+        private readonly int[] arrayA = { 8, 10, 14, 9, 12, 7 };
+        private readonly double[] arrayB = { 3.4, 4.2, 1.8, 5, 3.8 };
+
+        [TestMethod()]
+        public void Variance_Test()
+        {
+            var finalVariance = StatVariance.Variance(arrayA);
+            Assert.AreEqual(6.8, finalVariance);
+        }
+
+
+        [TestMethod()]
+        public void VarianceDoubleTest()
+        {
+            var finalVariance = StatVariance.Variance(arrayB);
+            Assert.AreEqual(1.408, finalVariance);
+        }
+    }
+}
