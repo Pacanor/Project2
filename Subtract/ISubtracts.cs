f9ebce53a3a8967f60c434799bfb12db518f972a
abigail <abigailnankamba@gmail.com>
2020-01-16 17:19:21 +0200

    Fix: Fixed the Subtract Project

---
+++ Subtract/ISubtracts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BasicOperations;
+
+namespace Subtract
+{
+    public interface ISubtracts
+    {
+        public dynamic Subtracted(dynamic a, dynamic b);
+        public dynamic Subtracted(dynamic a);
+
+    }
+}
