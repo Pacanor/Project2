9d22816ee71378f7b3947eea0c6aea61750905aa
MalamaKangwa <MalamaKangwa@szi.gov.zm>
2020-01-16 17:07:01 +0200

    Task: Square Root Feature Edited

---
+++ BasicOperations/SquareRoots.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Helpers;
+
+namespace BasicOperations
+{
+    public class SquareRoot
+    {
+        public static double Root(int a)
+        {
+            double b = Math.Sqrt(a);
+            return b;
+        }
+
+        public static double Root(double a)
+        {
+            double b = Math.Sqrt(a);
+            return b;
+        }
+
+
+        public static double[] Root(double[] arrayA)
+        {
+
+            int l = arrayA.Length;
+            double[] c = new double[l];
+            int i = 0;
+
+            foreach (double a in arrayA)
+            {
+                c[i] = Root(a);
+                i++;
+            }
+            return c;
+        }
+
+        public static double[] Root(int[] arrayA)
+        {
+
+            int l = arrayA.Length;
+            double[] c = new double[l];
+            int i = 0;
+
+            foreach (int a in arrayA)
+            {
+                dynamic x = Root(a);
+                dynamic e = Helpers.Rounding.RoundOffTwoPlaces(x);
+                c[i] = e;
+                i++;
+            }
+            return c;
+        }
+    }
+}
