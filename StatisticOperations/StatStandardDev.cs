6fadaf7d688964b96e3f62a38089a593e8d22e4f
MalamaKangwa <MalamaKangwa@szi.gov.zm>
2020-01-16 18:17:41 +0200

    Task: Edited Standard Deviations

---
+++ StatisticOperations/StatStandardDev.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticOperations
+{
+    public class StatStandardDev
+    {
+        public static double result;
+        public static double StandardDeviation(double[] arrayA)
+        {
+
+            dynamic theVariance = StatVariance.Variance(arrayA);
+
+            double Result1 = Math.Sqrt(theVariance);
+            result = Math.Round(Result1, 2, MidpointRounding.ToEven);
+            return result;
+        }
+
+        public static double StandardDeviation(int[] arrayA)
+        {
+            dynamic theVariance = StatVariance.Variance(arrayA);
+
+            double Result1 = Math.Sqrt(theVariance);
+            result = Math.Round(Result1, 2, MidpointRounding.ToEven);
+            return result;
+        }
+    }
+}
