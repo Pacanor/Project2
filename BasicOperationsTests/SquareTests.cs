80b60f2ec5e556bab4c6d5f6e807d8d8dc029a82
abigail <abigailnankamba@gmail.com>
2020-01-16 17:40:41 +0200

    Fix: Made a new Square

---
+++ BasicOperationsTests/SquareTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicOperations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicOperations.Tests
+{
+    [TestClass()]
+    public class SquareTests
+    {
+        private readonly double c = 2.5;
+        private readonly int a = 4;
+        [TestMethod()]
+        public void SquaredTest()
+        {
+            Assert.AreEqual(6.25, BasicOperations.Square.Squared(c));
+        }
+
+        [TestMethod()]
+        public void SquaredTest1()
+        {
+            Assert.AreEqual(16, BasicOperations.Square.Squared(a));
+        }
+    }
+}
---
+++ Squared/ISquaredC.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Squared
+{
+   public interface ISquaredC
+    {
+        public dynamic Squarea(dynamic a);
+    }
+}
