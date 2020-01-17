7c7d392be7274b694667d4a7900a92773af8472e
pacanor <papiasb@gmail.com>
2020-01-17 09:49:57 +0200

    Feature: Created Object to call Mode statics

---
+++ StatMode/FindMode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StatMode
+{
+    public class FindMode : IFindMode
+    {
+        public static dynamic result;
+
+        public dynamic Mode(dynamic a)
+        {
+            result = StatisticOperations.StatMode.Mode(a);
+            return result;
+        }
+    }
+}
+
