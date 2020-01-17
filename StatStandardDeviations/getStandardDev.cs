ac96bb671f5f062c73e38c0555c95cd12e1216b7
pacanor <papiasb@gmail.com>
2020-01-17 11:35:01 +0200

    Fix: Removed HelpersTests2 and renamed Standard Deviation folder

---
+++ StatStandardDeviations/getStandardDev.cs
@@ -0,0 +1,15 @@
+﻿using StatisticOperations;
+
+namespace StatStandardDeviations
+{
+    public class getStandardDev : IStandardDev
+    {
+        public static double result;
+
+        public dynamic StandardDeviation(dynamic arrayA)
+        {
+            result = StatStandardDev.StandardDeviation(arrayA);
+            return result;
+        }
+    }
+}
