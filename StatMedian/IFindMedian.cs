a8fcb6f82206d756832f6a653c2714f2d7859419
pacanor <papiasb@gmail.com>
2020-01-17 07:44:19 +0200

    Feature: Created object and interface to find Median

---
+++ StatMedian/IFindMedian.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatMedian
+{
+    interface IFindMedian
+    {
+        public dynamic Median(dynamic a);
+    }
+}
